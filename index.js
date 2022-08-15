efc626584321b3dc4bfcd5b17f2c224a00903e21
Kolonel1 <victor.munyao@student.moringaschool.com>
2022-08-15 13:42:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser='victor';
+const welcomeMessage='Welcome to Flatbook, '+currentUser+'!';
+const excitedWelcomeMessage='WELCOME TO FLATBOOK, '+ currentUser.toUpperCase()+'!';
+const shortGreeting='Welcome, '+ currentUser.slice(0,1) + '!';
+console.log(shortGreeting);
+
+// only concatenation can be used to pass the lab tests
